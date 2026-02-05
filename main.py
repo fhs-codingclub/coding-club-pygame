572776f3d288b85427b32fad4ddff3abe6003b78
ajtabjs <ajsw@mailhaven.su>
2026-02-05 01:01:50 +0000

    Added main menu; Seems screen is not working, somehow make menu be True iggggg like gng MWAHAHAHAHAHAHA Erik Shavesheads

--- main.py
+++ main.py
@@ -2,20 +2,42 @@
 import sys
 from loader import run_transition, WIDTH, HEIGHT
 from combat import run_battle
+from menu import main_menu
+
+start = True
 
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+def play():
     pygame.display.set_caption("DEBUG!!!")
-    
-    # Run the transition/loader first
+    # Run the transition/loader second MWAHAHAHAHAHAHHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAAHAHAHAhAHAHAHAhAHAHAHAHAHAhAHAHAHAhAHAHAHAHAHAHHAHAHAHAHAHAHAHAHAHAHAHA Help me
     if run_transition(screen):
         # If transition completed (didn't quit), start the battle
         run_battle(screen)
+        
+def main():
+    pygame.init()
+    pygame.display.set_caption("DEBUG!!!")
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
     
-    pygame.quit()
-    sys.exit()
+
+    #Run the main menu first
+    if start:
+        my_menu = main_menu(screen, play)
+        
+        run_menu = True
+        while my_menu.is_enabled():
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit
+            if my_menu.is_enabled():
+                my_menu.update(events)
+                my_menu.draw(screen)
+                pygame.display.update()
+            else:
+                run_menu = False
+    play()
 
 
 if __name__ == "__main__":
---
+++ menu.py
@@ -0,0 +1,35 @@
+from time import sleep
+import pygame
+import pygame_menu
+from pygame_menu import themes
+
+#Theme colors yAY!
+MY_CUSTOM_THEME = pygame_menu.themes.THEME_DARK.copy()
+BG_COLOR = (30, 30, 60)
+TITLE_FONT = pygame_menu.font.FONT_BEBAS
+FONT_COLOR = (215, 215, 0)
+TITLE_BG_COLOR = (20, 20, 40)
+BAR_STYLE = pygame_menu.widgets.MENUBAR_STYLE_UNDERLINE
+
+#Widget styles bleh
+THEME_WIDGET_FONT = pygame_menu.font.FONT_COMIC_NEUE
+WIDGET_FONT_COLOR = (200, 200, 200)
+THEME_WIDGET_ALIGNMENT = pygame_menu.locals.ALIGN_CENTER
+
+def main_menu(surface, start_callback):
+	#Start callback function that runs when "Play" is clicked
+	La_carte = pygame_menu.Menu(
+	title="PYGAME YESSIR",
+	width=surface.get_width(),
+	height=surface.get_height(),
+	theme=MY_CUSTOM_THEME
+	)
+	
+	#UI ELEMENTS
+	La_carte.add.label("SUP DUDE", font_size=30)
+	La_carte.add.vertical_margin(20)
+	La_carte.add.button("Start Game", La_carte.disable)
+	La_carte.add.button("Options", lambda: print("Options Menu Open"))
+	La_carte.add.button("QUIT", pygame_menu.events.EXIT)
+	
+	return La_carte
