79e02ff3afd0bc4e25889c910d4b47fa353cb5f5
aj <134237782+ajtabjs@users.noreply.github.com>
2026-04-15 23:45:27 +0000

    fix bash sccripts for codespaces!

--- scripts/fix-packages.sh
+++ scripts/fix-packages.sh
@@ -9,10 +9,3 @@ sudo gpg --batch --export 62D54FD4003F6525 2>/dev/null | sudo tee /usr/share/key
 echo "Installing dependencies..."
 sudo apt-get update -y
 sudo apt-get install -y xvfb x11vnc fluxbox websockify novnc
-
-echo "Starting virtual display..."
-export XDG_RUNTIME_DIR=/tmp/runtime-$(whoami)
-mkdir -p "$XDG_RUNTIME_DIR"
-Xvfb :1 -screen 0 1024x768x24 &
-export DISPLAY=:1
-fluxbox &
